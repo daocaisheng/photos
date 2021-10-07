08b178e442d606c323438501b8d257af0661e438
daocaisheng <deyusizehuiyusi@outlook.com>
2021-10-07 23:57:29 +0800

    添加文件改文件名

---
+++ 去文件名括号空格.bat
@@ -0,0 +1,14 @@
+@Echo Off&SetLocal ENABLEDELAYEDEXPANSION
+FOR %%a in (*) do (
+set "name=%%a"
+set "name=!name:(=!"
+set "name=!name:)=!"
+ren "%%a" "!name!"
+)
+
+@echo off
+Setlocal Enabledelayedexpansion
+set "str= "
+for /f "delims=" %%i in ('dir /b *.*') do (
+set "var=%%i" & ren "%%i" "!var:%str%=!")
+exit
